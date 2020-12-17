3105148401a221bacc5824899ecb011fe686e1a8
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-17 16:38:50 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/de/hshl/isd/quiz_assignment_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.hshl.isd.quiz_assignment_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_assignment_1/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
